d68928ee089f4789f9f3856b42a262676c426a76
Gustavo <gusstavocta@gmail.com>
2025-06-01 23:32:54 -0500

    llamando data desde la api moviedb

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,5 +1,6 @@
-
+import 'package:cinemapedia/presentation/providers/movies/movies_providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeScreen extends StatelessWidget {
   static const String name = 'home-screen';
@@ -9,7 +10,45 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Placeholder(),
+      body: _HomeView(),
     );
   }
+}
+
+class _HomeView extends ConsumerStatefulWidget {
+  const _HomeView();
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<_HomeView> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+    return ListView.builder(
+      itemCount: nowPlayingMovies.length,
+      itemBuilder: (context, index) {
+        final movie = nowPlayingMovies[index];
+        return ListTile(
+          title: Text(movie.title),
+          leading: movie.posterPath != null
+              ? Image.network(
+                  'https://image.tmdb.org/t/p/w500${movie.posterPath}',
+                  width: 50,
+                  height: 75,
+                  fit: BoxFit.cover,
+                )
+              : const Icon(Icons.movie),
+        );
+      },
+    );
+  }
+  // Load more movies when reaching the end of the list
 }
